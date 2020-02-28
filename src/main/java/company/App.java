230142699c5b814e434d7823292c8b8f003f3e75
liikii <609957891@qq.com>
2020-02-28 11:29:44 +0800

    up

--- src/main/java/company/App.java
+++ src/main/java/company/App.java
@@ -1,14 +1,12 @@
 package company;
 import com.blade.Blade;
-/**
- * Hello world!
- *
- */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	Blade.of().get("/", ctx -> ctx.text("Hello Blade")).start();
+	   Blade.of().get("/", ctx -> ctx.text("Hello Blade")).start();
     }
 }
